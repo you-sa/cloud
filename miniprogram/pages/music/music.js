e59d937f523743d6f38681b640647f4bda5bae95
you-sa <2930507371@qq.com>
2021-01-26 22:50:27 +0800

    组件

---
+++ miniprogram/pages/music/music.js
@@ -0,0 +1,127 @@
+// pages/music/music.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      imgUrls:[{
+        url:'http://p1.music.126.net/q5rKcBx9Y0V37DsUSaQKXg==/109951165664695730.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/WOoIZuva_umxxzYOvWINLA==/109951165664707565.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/C9I9GxpvRX7nCZyXNBeqOw==/109951165664694558.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/UdSM2BmqY_h_t9HAOzb5dQ==/109951165664710664.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/Z90NF2dHuBYrV6x-U9jJJQ==/109951165664719544.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/vAjwukVm-H0LOqzy4FTJXA==/109951165664851877.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/j0gp3gBDRRoqIXxAs0v7oA==/109951165664720877.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/9Ayx-EeCnuLRWKTcIhGB6g==/109951165664742856.jpg?imageView&quality=89'
+      }
+    ],
+    playlist:[{
+      "id":"1001",
+      "playCount":1.4641238e+06,
+      "name":"我试着把孤独藏进耳机",
+      "picUrl":"https://p2.music.126.net/Xvo6PwBcdOA69ipcpV9YYg==/109951165463253777.jpg?param=140y140"
+    },
+    {
+      "id":"1002",
+      "playCount":1.4641238e+04,
+      "name":"你一定要在自己热爱的世界里闪闪发亮呀",
+      "picUrl":"https://p2.music.126.net/uesfHcJmZ23S3er_1mpeaw==/109951165621856219.jpg?param=140y140"
+    },
+    {
+      "id":"1003",
+      "playCount":1.4641238e+06,
+      "name":"喜欢就要直接见面",
+      "picUrl":"https://p2.music.126.net/_rFjQsgRJfTQT55Wuv7T2Q==/109951165660082678.jpg?param=140y140"
+    },
+    {
+      "id":"1004",
+      "playCount":3.4641238e+06,
+      "name":"有幸被爱，余生是你",
+      "picUrl":"https://p2.music.126.net/0u7dhriB29wrDp_GyCrLFw==/109951165664303246.jpg?param=140y140"
+    },
+    {
+      "id":"1005",
+      "playCount":2.4561238e+06,
+      "name":"温柔不是我说，而是你觉得",
+      "picUrl":"https://p2.music.126.net/PJylNWy_2-jI7LRgQ2Cm6w==/109951165649129522.jpg?param=140y140"
+    },
+    {
+      "id":"1006",
+      "playCount":1.41238e+03,
+      "name":"心里装着银河星河和爱的人",
+      "picUrl":"https://p2.music.126.net/O8LkkfC7PtV7TA4UP693XA==/109951164569667332.jpg?param=140y140"
+    }
+  ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
